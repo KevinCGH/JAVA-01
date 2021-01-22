5ae5025aa598d7e481cc8926a78855ebd633a083
KevinChen <cgh804@163.com>
2021-01-22 21:48:07 +0800

    experiment file change

--- Week_02/src/main/java/com/kevin/java/course/nio01/HttpServer02.java
+++ Week_02/src/main/java/com/kevin/java/course/nio01/HttpServer02.java
@@ -1,10 +1,9 @@
 package com.kevin.java.course.nio01;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import static com.kevin.java.course.nio01.HttpUtil.service;
 
 /**
  * 多线程http服务器 8802
@@ -19,6 +18,23 @@ public class HttpServer02 {
             new Thread(() -> {
                 service(socket);
             }).start();
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+//            Thread.sleep(5);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio1";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) { // | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
--- Week_02/src/main/java/com/kevin/java/course/nio01/HttpServer03.java
+++ Week_02/src/main/java/com/kevin/java/course/nio01/HttpServer03.java
@@ -1,12 +1,12 @@
 package com.kevin.java.course.nio01;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static com.kevin.java.course.nio01.HttpUtil.service;
 
 /**
  * 多线程（线程池）http服务器 8803
@@ -24,6 +24,23 @@ public class HttpServer03 {
             final Socket socket = serverSocket.accept();
             System.out.println("server connect");
             executorService.execute(() -> service(socket));
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+//            Thread.sleep(5);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio1";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) { // | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
