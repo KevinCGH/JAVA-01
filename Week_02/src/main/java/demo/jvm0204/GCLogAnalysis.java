1bc84e439c2e8b2048051f8a7934f27e2273fe42
KevinChen <cgh804@163.com>
2021-04-16 23:56:27 +0800

    Modify project config
    - Week01
    - Week02

--- Week_02/src/main/java/demo/jvm0204/GCLogAnalysis.java
+++ Week_02/src/main/java/demo/jvm0204/GCLogAnalysis.java
@@ -1,13 +1,15 @@
-package com.kevin.java.course.gc;
+package demo.jvm0204;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
-/*
-演示GC日志生成与解读
-*/
+
+/**
+ * 演示 GC 日志生成与解读
+ */
 public class GCLogAnalysis {
     private static Random random = new Random();
+
     public static void main(String[] args) {
         // 当前毫秒时间戳
         long startMillis = System.currentTimeMillis();
@@ -17,20 +19,22 @@ public class GCLogAnalysis {
         long endMillis = startMillis + timeoutMillis;
         LongAdder counter = new LongAdder();
         System.out.println("正在执行...");
+
         // 缓存一部分对象; 进入老年代
         int cacheSize = 2000;
         Object[] cachedGarbage = new Object[cacheSize];
+
         // 在此时间范围内,持续循环
         while (System.currentTimeMillis() < endMillis) {
             // 生成垃圾对象
-            Object garbage = generateGarbage(100*1024);
+            Object garbage = generateGarbage(100 * 1024);
             counter.increment();
             int randomIndex = random.nextInt(2 * cacheSize);
             if (randomIndex < cacheSize) {
                 cachedGarbage[randomIndex] = garbage;
             }
         }
-        System.out.println("执行结束!共生成对象次数:" + counter.longValue());
+        System.out.println("执行结束!共生成对象次数: " + counter.longValue());
     }
 
     // 生成对象
@@ -54,7 +58,7 @@ public class GCLogAnalysis {
                 while (builder.length() < randomSize) {
                     builder.append(randomString);
                     builder.append(max);
-                    builder.append(randomSize);
+                    builder.append(randomString);
                 }
                 result = builder.toString();
                 break;
